5408e3dbefe3f7f0fccbd858a7e85a399f55998c
David Luu <dluu.audeo@gmail.com>
2022-01-29 09:45:35 -0800

    revised post code in server.js; notes now persist on browser

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/api/notes', (req, res) =>
@@ -27,23 +25,31 @@ app.get("*", (req, res) =>
 
 app.post("/api/notes", (req, res) => {
     const { title, text, id} = req.body;
-
-    const newNote = {title, text, id: uniqid()};
-
-    let notesArray = JSON.parse(db);
-    console.log(notesArray);
     
-    notesArray.push(newNote);
-    // Convert the data to a string so we can save it
-    const reviewNotes = JSON.stringify(notesArray);
-    // Write the string to a file
-    fs.writeFile(`./db/db.json`, reviewNotes, (err) =>
-        err ?
-        console.error(err) 
-        :console.log(`Review for ${newNote.title} has been written to JSON file`)
-    ); 
+    if( title&&text) {
+        // const notesArray = JSON.parse(db);
+        const newNote = {title, text, id: uniqid()};
+        
+        db.push(newNote);
+        
+        // const reviewNotes = JSON.parse(db);
+        
+        fs.writeFile(`./db/db.json`, db, (err) =>
+            err ?
+            console.error(err) 
+            :console.log(`Review for ${newNote.title} has been written to JSON file`)
+        ); 
 
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+  
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting notes');
+    }
 });
 
 app.listen(PORT, () =>
