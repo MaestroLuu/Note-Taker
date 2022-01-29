39c74dab5b2825091c325f792e92a45919f0d8d1
David Luu <dluu.audeo@gmail.com>
2022-01-28 22:47:14 -0800

    added code to post data to db.json

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-// const api = require("./public/assets/js/index.js");
+const api = require("./public/assets/js/index.js");
 const db = require("./db/db.json");
+const uuid = require("uniqid");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.use('/api', api);
-// app.use(express.json());
-// app.use(express.urlencoded({
-//     extended: true
-// }));
+app.use('/api', api);
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
-app.get('/api/notes', (req, res) => 
+app.get('/api/notes', (req, res) =>
     res.json(db)
 );
 
@@ -26,6 +27,24 @@ app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+app.post("/api/notes", (req, res) => {
+    const { title, text, id} = req.body;
+
+    const newNote = {
+        title,
+        text,
+        id: uniqid()
+    };
+    // Convert the data to a string so we can save it
+    const reviewNote = JSON.stringify(newNote);
+    // Write the string to a file
+    fs.writeFile(`./db/db.json`, reviewNote, (err) =>
+        err ?
+        console.error(err) 
+        :console.log(`Review for ${newNote.title} has been written to JSON file`)
+    ); 
+});
+
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http: //localhost:${PORT} 🚀`)
 );
