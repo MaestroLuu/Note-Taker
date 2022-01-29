b7927342e50512334f0fecac138e4e90007e6433
David Luu <dluu.audeo@gmail.com>
2022-01-28 19:57:26 -0800

    testing window theory

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-// const api = require("./public/assets/js/index.js");
+const api = require("./public/assets/js/index.js");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use('/api', api);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -14,11 +15,10 @@ app.get("/", (req, res) =>
 res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get("/api/notes", (req, res) =>
+app.get("/notes", (req, res) =>
 res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// app.use('/api', api);
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
