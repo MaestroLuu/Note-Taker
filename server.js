3d3a7afabb6f0c7a2fc8f0888271b0353ba1aa14
David Luu <dluu.audeo@gmail.com>
2022-01-28 21:34:37 -0800

    routed db.json so that it displays prior notes

--- server.js
+++ server.js
@@ -2,23 +2,30 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 // const api = require("./public/assets/js/index.js");
+const db = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // app.use('/api', api);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended: true
+// }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>
-res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get('/api/notes', (req, res) => 
+    res.json(db)
 );
 
 app.get("/notes", (req, res) =>
-res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
